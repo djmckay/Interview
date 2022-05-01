ba14edb0bfb885d4a2912f72421105e1d6eb0e59
DJ <djmckay@mac.com>
2022-05-01 17:13:06 -0400

    add si units option

--- src/main/java/tech/djmckay/demo/repo/WeatherRepoImpl.java
+++ src/main/java/tech/djmckay/demo/repo/WeatherRepoImpl.java
@@ -13,11 +13,16 @@ public class WeatherRepoImpl implements WeatherRepo {
 
 	@Value("${WEATHER_SERVER_URL}")
 	private String url;
+	
+	@Value("${WEATHER_SERVER_UNITS:}")
+	private String units;
 
 	@Override
 	public Mono<Weather> getDaily() {
+		String endpoint = units.isEmpty() ? "gridpoints/MLB/33,70/forecast" : "gridpoints/MLB/33,70/forecast?units="+units;
+		
 		WebClient client = WebClient.create(url);
-		Mono<Weather> result = client.get() .uri("gridpoints/MLB/33,70/forecast") 
+		Mono<Weather> result = client.get() .uri(endpoint) 
 		.accept(MediaType.APPLICATION_JSON) 
 		.retrieve() // Send request
 		.bodyToMono(Weather.class);
